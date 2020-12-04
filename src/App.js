283c5d063a9303a6c3a35841e22e78e996430238
Ketan <ketantoo@gmail.com>
2020-12-04 23:42:08 +0530

    deployment

--- src/App.js
+++ src/App.js
@@ -1,31 +1,37 @@
 import React, { useState } from "react";
 import Axios from "axios";
+import APIKEY from "./keys/keys";
 
-const APIKEY = "your api key";
 export default function App() {
   const [searchText, setSearchText] = useState("");
   const [films, setFilms] = useState([]);
+  const [isSearching, setIsSearching] = useState(false);
 
   const handleSubmit = (event) => {
     event.preventDefault();
+    setIsSearching(true);
+    setFilms([]);
+
     if (!searchText) {
       alert("Please enter movie name");
       return;
     }
     Axios.get(
-      `http://www.omdbapi.com/?i=tt3896198&apikey=${APIKEY}&s=${searchText}`
+      `http://www.omdbapi.com/?i=tt3896198&apikey=62316cb7&s=${searchText}`
     )
       .then((response) => {
         console.log(response.data);
         setFilms(response.data.Search);
+        setIsSearching(false);
       })
       .catch((error) => {
         console.log(error);
+        setIsSearching(false);
       });
   };
   return (
     <div className="App">
-      <form onSubmit={handleSubmit}>
+      <form className="input" onSubmit={handleSubmit}>
         <input
           placeholder={"Search movie"}
           onChange={(event) => {
@@ -35,13 +41,20 @@ export default function App() {
         <button>Search</button>
       </form>
       <div className="movieContainer">
-        {films.map((film) => (
-          <div className="filmCard">
-            <img src={film["Poster"]} width={200} alt={film["Title"]} />
-            <p>{film["Title"]}</p>
-            <p>{film["Year"]}</p>
-          </div>
-        ))}
+        {isSearching ? (
+          <p>Loading</p>
+        ) : (
+          films.map((film) => (
+            <div className="filmCard">
+              <img src={film["Poster"]} width={200} alt={film["Title"]} />
+              <div className="textBox">
+                {" "}
+                <p className="title">{film["Title"]}</p>
+                <p className="year">Released: {film["Year"]}</p>
+              </div>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
